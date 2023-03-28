fd002685d24cb43d1e76bc8dafec0fb3a503859f
Fennikz <tripop.xtarnos@mail.kmutt.ac.th>
2023-03-28 23:58:06 +0700

    first commit

---
+++ Q2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PointF> polygon = new List<PointF>();
+
+            while (true)
+            {
+                Console.Write("Enter x and y (separated by a space): ");
+                string[] input = Console.ReadLine().Split();
+                float x = float.Parse(input[0]);
+                float y = float.Parse(input[1]);
+
+                if (x == 0 && y == 0)
+                {
+                    break;
+                }
+
+                polygon.Add(new PointF(x, y));
+            }
+
+            Console.Write("Enter the point to be tested (x and y separated by a space): ");
+            string[] pointInput = Console.ReadLine().Split();
+            float pointX = float.Parse(pointInput[0]);
+            float pointY = float.Parse(pointInput[1]);
+            PointF point = new PointF(pointX, pointY);
+
+            bool isInside = false;
+            int j = polygon.Count - 1;
+
+            for (int i = 0; i < polygon.Count; i++)
+            {
+                if ((polygon[i].Y < point.Y && polygon[j].Y >= point.Y || polygon[j].Y < point.Y && polygon[i].Y >= point.Y) &&
+                    (polygon[i].X + (point.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) * (polygon[j].X - polygon[i].X) < point.X))
+                {
+                    isInside = !isInside;
+                }
+
+                j = i;
+            }
+
+            // Output the result
+            if (isInside)
+            {
+                Console.WriteLine("Inside");
+            }
+            else
+            {
+                Console.WriteLine("Outside");
+            }
+        }
+    }
---
+++ Q3/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program {
+    static void Main(string[] args) {
+        double x1, y1, x2, y2, r1, r2;
+
+        Console.WriteLine("Input center of circle C1");
+        x1 = double.Parse(Console.ReadLine());
+        y1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Input center of circle C2");
+        x2 = double.Parse(Console.ReadLine());
+        y2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Input radius of circle C1");
+        r1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Input radius of circle C2");
+        r2 = double.Parse(Console.ReadLine());
+
+        double distance = Math.Sqrt(Math.Pow(x2 - x1, 2f) + Math.Pow(y2 - y1, 2f));
+
+        if (distance <= r1 + r2) {
+            double a = (r1 * r1 - r2 * r2 + distance * distance)/(2*distance);
+            double h = Math.Sqrt(r1 * r1 - a * a);
+            
+            //P2
+            double x3 = x1 + a * (x2 - x1) / distance;
+            double y3 = y1 + a * (y2 - y1) / distance;
+            //intersectionPoint1
+            double x4 = x3 - h * (y2 - y1) / distance;
+            double y4 = y3 + h * (x2 - x1) / distance;
+            //intersectionPoint2
+            double x5 = x3 + h * (y2 - y1) / distance;
+            double y5 = y3 - h * (x2 - x1) / distance;
+
+            if(distance == r1 + r2)
+            {
+                Console.WriteLine(x4);
+                Console.WriteLine(y4);
+            }
+            else
+            {
+                Console.WriteLine(x4);
+                Console.WriteLine(y4);
+                Console.WriteLine(x5);
+                Console.WriteLine(y5);
+            }
+
+        }
+    }
+}
